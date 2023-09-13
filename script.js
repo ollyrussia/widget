5b19bbe4449e2e1717bc319b735629a3acef7c75
ollyrussia <135809030+ollyrussia@users.noreply.github.com>
2023-09-13 14:15:02 +0200

    v1.1.0
    Первая рабочая версия с хотфиксом валидации

--- script.js
+++ script.js
@@ -53,6 +53,30 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
       }).then(response => response)
     }
 
+    function widget_get_services_by_id_yclients(id){
+      return fetch('https://api.yclients.com/api/v1/company/542516/services?staff_id='+id, {
+        method: 'GET',
+        headers: {
+          "Authorization": "Bearer tzx2jrfxx6cdhxafcb6g, User c912ed93d93fdc1c165456baf618bf07",
+          "Accept": "application/vnd.yclients.v2+json",
+          "Content-Type": "application/json"
+        }
+      }).then(response => response.json())
+    }
+
+    function widget_create_record_on_yclients(data){
+      console.log(data);
+      return fetch('https://api.yclients.com/api/v1/records/542516/', {
+        method: 'POST',
+        body: data,
+        headers: {
+          "Authorization": "Bearer tzx2jrfxx6cdhxafcb6g, User c912ed93d93fdc1c165456baf618bf07",
+          "Accept": "application/vnd.yclients.v2+json",
+          "Content-Type": "application/json"
+        }
+      }).then(response => response.json())
+    }
+
     this.callbacks = {
       render: function () {
 
@@ -64,19 +88,19 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
             render: '<div class="widget-body-free-slots-yclients"></div>'
         }, {});
 
-        console.log('pre-render');
         return true;
       },
       init: _.bind(function () {
         console.log('init');
+        //console.log($('td[name="tcol1"]').val());
+        //console.log($('.company-contacts').html());
 
         var settings = self.get_settings();
          //   Проверяем подключен ли наш файл css
-         console.log('link[href="' + settings.path + '/style.css?v=' + settings.version +'"');
-       // if ($('link[href="' + settings.path + '/style.css?v=' + settings.version +'"').length < 1) {
+        if ($('link[href="' + settings.path + '/style.css?v=' + settings.version +'"').length < 1) {
             //  Подключаем файл style.css передавая в качестве параметра версию виджета
             $("head").append('<link href="' + settings.path + '/style.css?v=' + settings.version + '" type="text/css" rel="stylesheet">');
-      //  }
+        }
         widget_get_psychologist_from_yclients()
             .then((data) => {
               psy_list = data.data;
@@ -107,7 +131,13 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
 
                 //alert( "User clicked on 'psy.' with id "+ psy_id+' | '+name+' | '+spec+' | '+ava);
                 
-                let list_header = '<div class="psy-schedule-header"><div class="back-to-psy-list"></div><div class="psy-schedule-header-data">';
+                let list_header = '<div class="psy-schedule-header">';
+                list_header += '<div class="back-to-psy-list"><svg fill="#45abb5" version="1.1" baseProfile="tiny" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"'+
+	 'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"'+
+	  'width="30px" height="30px" viewBox="0 0 42 42" xml:space="preserve">'+
+'<polygon fill-rule="evenodd" points="27.066,1 7,21.068 26.568,40.637 31.502,35.704 16.865,21.068 32,5.933 "/>'+
+'</svg></div>';
+                list_header += '<div class="psy-schedule-header-data">';
                 list_header += '<img class="psy-schedule-ava" src='+ava+' />';
                 list_header += '<span class="psy-schedule-name">'+name+'</span>';
                 list_header += '<span class="psy-schedule-spec">'+spec+'</span></div>';
@@ -119,7 +149,7 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
                 });
 
                 widget_get_records_by_id_yclients(psy_id).then((data) => {
-                  console.log(data);
+                  //console.log(data);
 
                   records_list = data.data;
     
@@ -176,10 +206,10 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
                   '{% endif %}' +
 
                   '{% if record.services[0].id == 13253375 %}' +
-                            '<div class="psy-base-box date-time-box" id="service-date-{{record.id}}" title="{{ record.services[0].title }}">{{ record.date|slice(10, 6) }}</div>' +
+                            '<div class="psy-base-box date-time-box" needed="base" date-time="{{record.date}}" staff-id="{{record.staff_id}}" id="service-date-{{record.id}}" title="{{ record.services[0].title }}">{{ record.date|slice(10, 6) }}</div>' +
                           '{% endif %}' +
                           '{% if record.services[0].id == 13253378 %}' +
-                            '<div class="psychosomatic-box date-time-box" id="service-date-{{record.id}}" title="{{ record.services[0].title }}">{{ record.date|slice(10, 6) }}</div>' +
+                            '<div class="psychosomatic-box date-time-box" needed="psycho" date-time="{{record.date}}" staff-id="{{record.staff_id}}" id="service-date-{{record.id}}" title="{{ record.services[0].title }}">{{ record.date|slice(10, 6) }}</div>' +
                   '{% endif %}' +
                   //'</div>'+
 
@@ -194,14 +224,25 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
                   $( ".date-time-box" ).bind( "click", function() {
                       
                       let service_id = $(this).attr('id').replace('service-date-', '');
-                      embedded_form = '<div class="back-to-psy-list"></div>'+
-                                      '<textarea class="embedded-form-field" name="query" placeholder="Введите запрос клиента"></textarea>'+
-                                      '<input class="embedded-form-field" name="name" type="text" placeholder="Введите имя фамилию клиента">'+
+                      let needed = $(this).attr('needed');
+                      let staff_id = $(this).attr('staff-id');
+                      let date_time = $(this).attr('date-time'); 
+
+                      let embedded_form = '<div class="back-to-psy-list"><svg fill="#45abb5" version="1.1" baseProfile="tiny" id="Layer_1" xmlns:x="&ns_extend;" xmlns:i="&ns_ai;" xmlns:graph="&ns_graphs;"'+
+                      'xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"'+
+                      'width="30px" height="30px" viewBox="0 0 42 42" xml:space="preserve">'+
+                      '<polygon fill-rule="evenodd" points="27.066,1 7,21.068 26.568,40.637 31.502,35.704 16.865,21.068 32,5.933 "/>'+
+                      '</svg></div>' +
+                                      '<input id="hidden-needed" value="'+needed+'" style="visibility:hidden;display:none;" />' +
+                                      '<input id="hidden-staff-id" value="'+staff_id+'" style="visibility:hidden;display:none;" />' +
+                                      '<input id="hidden-datetime" value="'+date_time+'" style="visibility:hidden;display:none;" />' +
+                                      '<textarea class="embedded-form-field" id="widget-free-windows-query" placeholder="Введите запрос клиента"></textarea>'+
+                                      '<input class="embedded-form-field" id="widget-free-windows-name" type="text" placeholder="Введите имя фамилию клиента">'+
                                       //'<input class="embedded-form-field" name="surname" type="text" placeholder="Введите фамилию клиента">'+
                                       //'<input class="embedded-form-field" name="birthdate" type="date" placeholder="Введите дату рождения клиента">'+
-                                      '<input class="embedded-form-field" name="email" type="text" placeholder="Введите email клиента">'+
-                                      '<input class="embedded-form-field" name="phone" type="text" placeholder="Введите телефон клиента">'+
-                                      '<input class="embedded-form-field" name="promo" type="text" placeholder="Введите промокод клиента">'+
+                                      '<input class="embedded-form-field" id="widget-free-windows-email" type="text" placeholder="Введите email клиента">'+
+                                      '<input class="embedded-form-field" id="widget-free-windows-phone" type="text" placeholder="Введите телефон клиента">'+
+                                      '<input class="embedded-form-field" id="widget-free-windows-promo" type="text" placeholder="Введите промокод клиента">'+
                                       '<button class="embedded-form-button" id="binded-to-'+service_id+'">Создать запись</button>';
                     
                       $('.widget-body-free-slots-yclients').html(embedded_form);
@@ -211,15 +252,81 @@ define(['jquery', 'underscore', 'twigjs'], function ($, _, Twig) {
                       });
                       $( ".embedded-form-button" ).bind( "click", function() {
                         let id = $(this).attr('id').replace('binded-to-', '');
-                        widget_delete_free_window_from_yclients(id).then((data) => {
-                             console.log(data);
-                        /*  create_new_record_on_yclients().then((data) => {
-                             console.log(data);
+                             
+                             let staff_id = $('#hidden-staff-id').val();
+                             let date_time = $('#hidden-datetime').val();
+                             let needed_service = $('#hidden-needed').val();
+        
+                             let name_field = $('#widget-free-windows-name').val();
+                             let phone_field = $('#widget-free-windows-phone').val();
+                             let email_field = $('#widget-free-windows-email').val();
+                             let comment_field = $('#widget-free-windows-query').val();
 
-                          });*/
-                          alert('Свободное окно удалено');
-                          self.callbacks.init();
-                        });
+                          widget_get_services_by_id_yclients(staff_id).then((data) => {
+                            
+                            let needed_service_id, needed_service_price, needed_service_duration;
+                            for(let i = 0; i < data.data.length; i++){
+                             
+                              if(data.data[i].booking_title.indexOf("Консультация с терапевтом") >= 0 
+                                 && needed_service == 'base'){
+                                needed_service_id = data.data[i].id;
+                                needed_service_price = data.data[i].price_max;
+                                needed_service_duration = 5400;
+                              }else if(data.data[i].booking_title.indexOf("Первичная консультация по психосоматике с терапевтом") >= 0 
+                                       && needed_service == 'psycho'){
+                                needed_service_id = data.data[i].id;
+                                needed_service_price = data.data[i].price_max;
+                                needed_service_duration = 10800;
+                              }
+                            }
+
+                            let record_data = JSON.stringify({
+                              staff_id: staff_id,
+                              services: [{ id: needed_service_id, 
+                                          first_cost: needed_service_price, 
+                                          discount: 0, 
+                                          cost: needed_service_price}],
+                              client:{ phone: phone_field,
+                                      name: name_field,
+                                      email: email_field},
+                              datetime: date_time,	
+                              seance_length: needed_service_duration,
+                              comment: comment_field
+                            });
+
+                            var approved = false;
+                            var err_message = 'Необходимые поля не заполнены';
+                            if(email_field != '' && phone_field == ''){
+                              approved = false;
+                              var emaileno = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+                              if(email_field.toLowerCase().match(emaileno)){
+                                approved = true;
+                                err_message = 'Неверный формат email.';
+                              }
+                            }
+                            if(phone_field != '' && email_field == ''){
+                              approved = false;
+                              if(phone_field > 10000000000 && phone_field < 100000000000){
+                                approved = true;
+                                err_message = 'Неверный формат номера.';
+                              }
+                            }
+                            if(approved){
+                              widget_delete_free_window_from_yclients(id).then((data) => {
+                                widget_create_record_on_yclients(record_data).then((data) => {
+                                  if(data.success){
+                                    alert('Запись успешно создана!');                      
+                                  }else{
+                                    alert(data.meta.message);
+                                  }
+                                  //console.log(data);
+                               });
+                              });
+                            }else{
+                              alert(err_message);
+                            }
+                          });
+                          self.callbacks.init();  
                       });
                   });
                 });
